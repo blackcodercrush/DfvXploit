d278a6ce2d1a235c1a724a3cc12accc21cf9ee0f
BlackCoderCrush <45306036+BlackCoderCrush@users.noreply.github.com>
2019-03-05 09:28:29 +0700

    Create menu.py

---
+++ core/menu.py
@@ -0,0 +1,31 @@
+a='\033[1;30m' # Ini Adalah Warna Abu - abu / Grey
+r='\033[1;31m' # Ini Adalah Warna Merah / red
+g='\033[32;1m' # Ini Adalah Warna Hijau / green
+y='\033[1;33m' # Ini Adalah Warna Kuning / yellow
+b='\033[1;34m' # Ini Adalah Warna Biru / blue
+p='\033[1;35m' # Ini Adalah Warna purple
+c='\033[1;36m' # Ini Adalah Warna cyan / biru terang
+w='\033[1;37m' # Ini Adalah Warna putih / white
+n='\033[0;00m' # Ini Adalah Warna normal / warna default termux
+o='\033[1;38;5;208m' # it's orange
+
+
+import time, os, sys
+
+def awal():
+	print (w+"                                         _________-----_____")
+	print (w+"                             _____------           __      ----_ ")
+	print (w+"                     ___----             ___------              \ ")
+	print (w+"                        ----________        ----                 \ ")
+	print (w+"                                    -----__    |             "+r+"_____"+w+") ")
+	print (w+"                                         __-                "+r+"/#####"+w+"\ ")
+	print (w+"                             _______-----    ___--          "+r+"\####/"+w+")\ ")
+	print (w+"                       ------_______      ---____           "+r+" \##/"+w+"  / ")
+	print (w+"                                    -----__    \ --    _    "+r+"  -- "+w+"  /\ ")
+	print (w+"                                           --__--__     \_____/   \_/\ ")
+	print (w+"                                                  ----|   /          | ")
+	print (w+"    * - =="+b+"[ "+g+"DfvXploit Attacker Tools              "+b+"]  "+w+"|  |___________| ")
+	print (w+"    * - =="+b+"[ "+w+"Author "+g+": "+w+"M.Daffa ( Dfv47 )            "+b+"]  "+w+"|  | ((_(_)| )_) ")
+	print (w+"    * - =="+b+"[ "+w+"Team   "+g+": "+w+"Black Coder Crush            "+b+"]  "+w+"|  \_((_(_)|/(_) ")
+	print (w+"    * - =="+b+"[ "+w+"Phone  "+g+": "+w+"daffamfthhsn21@gmail.com     "+b+"]  "+w+"\             ( ")
+	print (w+"                                                     \_____________) ")
