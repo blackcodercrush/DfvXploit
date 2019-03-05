7743e12be1b6051ac00bd78e53320418b5639639
BlackCoderCrush <45306036+BlackCoderCrush@users.noreply.github.com>
2019-03-05 09:27:21 +0700

    Create about.py

---
+++ core/about.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+a='\033[1;30m' # Ini Adalah Warna Abu - abu / Grey
+r='\033[1;31m' # Ini Adalah Warna Merah / red
+g='\033[32;1m' # Ini Adalah Warna Hijau / green
+y='\033[1;33m' # Ini Adalah Warna Kuning / yellow
+b='\033[1;34m' # Ini Adalah Warna Biru / blue
+p='\033[1;35m' # Ini Adalah Warna purple
+c='\033[1;36m' # Ini Adalah Warna cyan / biru terang
+w='\033[1;37m' # Ini Adalah Warna putih / white
+n='\033[0;00m' # Ini Adalah Warna normal / warna default termux
+o='\033[1;38;5;208m' # it's orange
+
+
+
+def about():
+	print (g+"           ¦")
+	print (g+"           ¦––––"+r+"► "+w+"Codename      "+r+": "+w+"@Dfv47")
+	print (g+"           ¦––––"+r+"► "+w+"Author        "+r+": "+w+"Miftahul Daffa Hasani")
+	print (g+"           ¦––––"+r+"► "+w+"Team          "+r+": "+w+"Black Coder Crush")
+	print (g+"           ¦––––"+r+"► "+w+"Phone         "+r+": "+w+"+6282223108828")
+	print (g+"           ¦––––"+r+"► "+w+"Github me     "+r+": "+w+"https://github.com/Dfv47")
+	print (g+"           ¦––––"+r+"► "+w+"Github team   "+r+": "+w+"https://github.com/BlackCoderCrush")
+	print (g+"           ¦––––"+r+"► "+w+"Email Me      "+r+": "+w+"daffamfthhsn21@gmail.com")
+	print (g+"           ¦––––"+r+"► "+w+"Email Team    "+r+": "+w+"blackcodercrush@gmail.com")
+	print (g+"           ¦––––"+r+"► "+w+"Website       "+r+": "+w+"https://www.blackcodercrush.zone.id")
+	print (g+"           ¦––––"+r+"► "+w+"Thanks To     "+r+": "+w+"All Member Black Coder Crush")	
+	print ""
