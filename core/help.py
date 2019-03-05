f00aed4b43d2efa1b12dec157dad939152329c16
BlackCoderCrush <45306036+BlackCoderCrush@users.noreply.github.com>
2019-03-05 09:31:09 +0700

    Create help.py

---
+++ core/help.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+a='\033[1;30m' # Ini Adalah Warna Abu - abu / Grey
+r='\033[1;31m' # Ini Adalah Warna Merah / red
+g='\033[32;1m' # Ini Adalah Warna Hijau / green
+y='\033[1;33m' # Ini Adalah Warna Kuning / yellow
+b='\033[1;34m' # Ini Adalah Warna Biru / blue
+p='\033[1;35m' # Ini Adalah Warna purple
+c='\033[1;36m' # Ini Adalah Warna cyan / biru terang
+w='\033[1;37m' # Ini Adalah Warna putih / white
+n='\033[0;00m' # Ini Adalah Warna normal / warna default termux
+o='\033[1;38;5;208m' # it's orange
+
+def help():
+    print (g+"           ¦")
+    print (g+"           ¦––––"+r+"► "+w+"ex_fabrik       "+r+": "+w+"Scan web with com fabrik exploit")
+    print (g+"           ¦––––"+r+"► "+w+"ex_flip         "+r+": "+w+"Scan web with my flip exploit")
+    print (g+"           ¦––––"+r+"► "+w+"ex_elfinder     "+r+": "+w+"Scan web with elfinder exploit")
+    print (g+"           ¦––––"+r+"► "+w+"ex_sitefinity   "+r+": "+w+"Scan web with sitefinity exploit")
+    print (g+"           ¦––––"+r+"► "+w+"ex_spawn        "+r+": "+w+"Scan web with spawn uploader exploit")
+    print (g+"           ¦––––"+r+"► "+w+"ex_colector     "+r+": "+w+"Scan web with com colector exploit")
+    print (g+"           ¦––––"+r+"► "+w+"ex_joomla       "+r+": "+w+"Scan web with cms joomla exploit")
+    print (g+"           ¦––––"+r+"► "+w+"ex_kind         "+r+": "+w+"Scan web with kind editor exploit")
+    print (g+"           ¦––––"+r+"► "+w+"ex_fck          "+r+": "+w+"Scan web with fck link exploit")   
+    print (g+"           ¦––––"+r+"► "+w+"ex_rfm          "+r+": "+w+"Scan web with responsive file manager exploit")
+    print (g+"           ¦––––"+r+"► "+w+"ex_timthumb     "+r+": "+w+"Scan web with timthumb rce exploit")
+    print (g+"           ¦––––"+r+"► "+w+"ex_cbt          "+r+": "+w+"Scan web with computer basic test exploit")
+    print (g+"           ¦")
+    print (g+"           ¦––––"+r+"► "+w+"help            "+r+": "+w+"Display help menu")
+    print (g+"           ¦––––"+r+"► "+w+"update          "+r+": "+w+"Update this tools")    
+    print (g+"           ¦––––"+r+"► "+w+"about           "+r+": "+w+"About creator tools")
+    print (g+"           ¦––––"+r+"► "+w+"exit            "+r+": "+w+"Exit program")
+    print ""
